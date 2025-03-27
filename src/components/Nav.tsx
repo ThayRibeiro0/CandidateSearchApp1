ed99c627277ae9797e375f4521fbe63ab1a8e517
Thays Ribeiro <thaysmoiaribeiro@gmail.com>
2025-03-27 08:52:28 -0700

    Add navigation links and styling to the Nav component

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <div>
+      <Link className="nav-a-home" to="/">Home</Link>
+      <Link className="nav-a-potencial-candidates" to="/potentialCandidates">Potential Candidates</Link>
+    </div>
   )
 };
 
