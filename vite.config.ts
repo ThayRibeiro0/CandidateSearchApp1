c473e457e566114835eb4c44f66f7e097838ee36
Thays Ribeiro Maher <94051605+ThayRibeiro0@users.noreply.github.com>
2025-03-29 14:27:05 +0000

    Updates server settings in Vite to use VITE_PORT and adds host restrictions

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: './environment',
   server: { 
-    port: Number(process.env.PORT) || 3000,
     host: '0.0.0.0',
+    port: Number(process.env.VITE_PORT) || 5173,
+    strictPort: true, 
+    allowedHosts: ['candidatesearchapp1.onrender.com']
   },
-  plugins: [react()],
+  plugins: [react()]
 });
