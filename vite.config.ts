a1a62756c890c1008d72b4970cf055ceb17bf939
Thays Ribeiro Maher <94051605+ThayRibeiro0@users.noreply.github.com>
2025-03-29 01:31:29 +0000

    Adiciona tipos para Node e configurações de servidor no Vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   envDir: './environment',
+  server: { 
+    port: Number(process.env.PORT) || 3000,
+    host: '0.0.0.0',
+  },
   plugins: [react()],
 });
